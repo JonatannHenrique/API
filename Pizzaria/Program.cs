3e83f9dc46804d22ebb8e9efa7a78acb76ec803e
Robson <robsonhobbye@gmail.com>
2025-08-15 15:40:38 -0300

    Colocando Port

--- Pizzaria/Program.cs
+++ Pizzaria/Program.cs
@@ -17,6 +17,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 
 var app = builder.Build();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
