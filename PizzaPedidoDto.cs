0f9ee0dd2155402d892f667d285fac7723314090
Henrique <luana.dabella2006@gmail.com>
2025-08-15 13:51:54 -0300

    Delete PizzaPedidoDto.cs

--- PizzaPedidoDto.cs
+++
@@ -1,7 +0,0 @@
-public class PizzaPedidoDto { 
-    public int Id { get; set; }
-    public int PizzaId { get; set; }
-    public DateTime Data { get; set; }
-    public DateTime Data { get; set; } 
-    public int Quantidade { get; set; }
-}
