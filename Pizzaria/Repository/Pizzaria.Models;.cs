d0dbb3e56d0b851e94d4da5b96fc6d17eec048b1
Henrique <luana.dabella2006@gmail.com>
2025-08-18 12:20:54 -0300

    Update Pizzaria.Models;.cs

--- Pizzaria/Repository/Pizzaria.Models;.cs
+++ Pizzaria/Repository/Pizzaria.Models;.cs
@@ -2,18 +2,18 @@
 using Pizzaria.Models;
 namespace Pizzaria.Repository
 {
-    internal class CadastroRepository : BaseRepositorio
-    {
-        public CadastroRepository(AppDbContext repository) : base(repository)
-        {
-        }
-        public async Task<Cadastro> GetLoginByasync(ulong id)
-        {
-            var logins = await _repository.Clientes
-             .AsNoTracking()
-             .FirstOrDefaultAsync(c => c.Id == id);
-            return logins;
-        }
-    }
+    //internal class CadastroRepository : BaseRepositorio
+    //{
+    //  public CadastroRepository(AppDbContext repository) : base(repository)
+    //    {
+    //   }
+    //    public async Task<Cadastro> GetLoginByasync(ulong id)
+    //    {
+    //        var logins = await _repository.Clientes
+    //         .AsNoTracking()
+    //         .FirstOrDefaultAsync(c => c.Id == id);
+    //        return logins;
+    //    }
+    //}
 }
 
