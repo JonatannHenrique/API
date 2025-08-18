a6e3aebba0d1f0fc444b16fc44afbc34b47e2a07
Henrique <luana.dabella2006@gmail.com>
2025-08-18 12:19:11 -0300

    Update Program.cs

--- Pizzaria/Program.cs
+++ Pizzaria/Program.cs
@@ -15,17 +15,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
     )
 );
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTudo",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
 
 var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Urls.Add($"http://*:{port}");
 
-// Configure the HTTP request pipeline.
+app.UseCors("PermitirTudo"); // Habilita o CORS com a política definida, Apenas para testes Locais
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.MapGet("/", () => "API ONLINE! ✅");
 app.UseHttpsRedirection();
 app.UseAuthorization();
